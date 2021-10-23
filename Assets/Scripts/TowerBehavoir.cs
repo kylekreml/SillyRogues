1e307372cf67989dcff4ad3009758b4ca7b0de8b
Brian Khuu <bhkhuu@uci.edu>
2021-10-22 20:25:23 -0700

    Added tower but there is a bug when the tower is placed just right

--- Assets/Scripts/TowerBehavoir.cs
+++ Assets/Scripts/TowerBehavoir.cs
@@ -11,13 +11,19 @@ public class TowerBehavoir : MonoBehaviour
 
     public GameObject bullet;
 
+    public int radius;
+
+    public GameObject target = null;
+
+    float targetDistance = 1000000;
+
 
     // Start is called before the first frame update
     void Start()
     {
         timer = bulletRespawn;
     }
-
+    /*
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
@@ -61,12 +67,106 @@ public class TowerBehavoir : MonoBehaviour
                 timer -= Time.deltaTime;
         }
     }
-
+    */
 
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        
+        if (timer <= 0)
+        {
+
+            if (target == null)
+            {
+                //only used on first enemy encountered
+                Collider2D[] objects = Physics2D.OverlapCircleAll(this.transform.position, radius);
+
+                if (objects.Length != 0)
+                {
+                    for (int i = 0; i < objects.Length; i++)
+                    {
+                        if (objects[i].gameObject.CompareTag("Enemy") && objects[i].gameObject.GetComponent<BasicEnemyScript>().RemainingDistance() < targetDistance)
+                        {
+                            target = objects[i].gameObject;
+                            targetDistance = objects[i].gameObject.GetComponent<BasicEnemyScript>().RemainingDistance();
+                        }
+                    }
+                }
+
+                if (target)
+                {
+                    Vector3 vectorToTarget = target.transform.position - transform.position;
+                    float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+                    Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+                    transform.rotation = q;
+                    GameObject newBullet = Instantiate(bullet, this.gameObject.transform.GetChild(0).position, Quaternion.identity);
+                    newBullet.GetComponent<ProjectileBehavoir>().setTarget(target);
+                    timer = bulletRespawn;
+
+                }
+
+            }
+
+            else
+            { //if timer is <= 0 AND target != null
+                if (Vector3.Distance(target.transform.position, this.transform.position) < radius)
+                //enemy out of range
+                { 
+
+                        Vector3 vectorToTarget = target.transform.position - transform.position;
+                        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+                        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+                        transform.rotation = q;
+                        GameObject newBullet = Instantiate(bullet, this.gameObject.transform.GetChild(0).position, Quaternion.identity);
+                        newBullet.GetComponent<ProjectileBehavoir>().setTarget(target);
+                        timer = bulletRespawn;
+                    
+                }
+                else
+                { //target out of range
+                    target = null;
+                    targetDistance = 1000000;
+
+                    Collider2D[] objects = Physics2D.OverlapCircleAll(this.transform.position, radius);
+
+                    if (objects.Length != 0)
+                    {
+                        for (int i = 0; i < objects.Length; i++)
+                        {
+                            if (objects[i].gameObject.CompareTag("Enemy") && objects[i].gameObject.GetComponent<BasicEnemyScript>().RemainingDistance() < targetDistance)
+                            {
+                                target = objects[i].gameObject;
+                                targetDistance = objects[i].gameObject.GetComponent<BasicEnemyScript>().RemainingDistance();
+                            }
+                        }
+                    }
+
+                    if (target)
+                    {
+                        Vector3 vectorToTarget = target.transform.position - transform.position;
+                        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+                        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+                        transform.rotation = q;
+                        GameObject newBullet = Instantiate(bullet, this.gameObject.transform.GetChild(0).position, Quaternion.identity);
+                        newBullet.GetComponent<ProjectileBehavoir>().setTarget(target);
+                        timer = bulletRespawn;
+
+                    }
+                }
+            }
+
+
+        }
+        else 
+        {
+            timer -= Time.deltaTime;
+        }
+
+
+
+
+
+
+
     }
 }
