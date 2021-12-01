18d68f5abd1d096987ff801545092031bc96e122
JC8299 <justincheng.cheng@gmail.com>
2021-12-01 13:02:56 -0800

    Added a pause menu
    Main menu button doesn't work if Title Scene is not active in the build settings. Have not tested a potential bug with the resume button (clicking and hitting the esc key at the same time).

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    private bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+        transform.GetChild(0).gameObject.SetActive(paused);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        GameManager.Instance.Pause(true);
+        paused = !paused;
+        transform.GetChild(0).gameObject.SetActive(paused);
+    }
+
+    public void MainMenu()
+    {
+        GameManager.Instance.ChangeScene("Title Scene");
+    }
+}
