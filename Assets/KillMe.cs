7db029c53eb244d11028659fe4644077cac9d8c0
Aipysurus <54886633+Aipysurus@users.noreply.github.com>
2021-12-06 20:24:07 -0800

    honestly I forgot

---
+++ Assets/KillMe.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillMe : MonoBehaviour
+{
+    public float time = 5;
+    private float timeSinceStart = 0;
+    public PauseMenuScript pause;
+    Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause.Pause();
+        slider = this.transform.Find("time").gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = Time.realtimeSinceStartup / time;
+        if (Time.realtimeSinceStartup >= time)
+        {
+            if(Input.anyKeyDown)
+            {
+                pause.Pause();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
