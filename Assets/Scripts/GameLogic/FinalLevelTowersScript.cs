162b13e6b4623ca62b0dc86b6197dec386c8fa6f
JC8299 <justincheng.cheng@gmail.com>
2022-03-02 19:00:53 -0800

    Added final level capability
    Added carrying towers between  scenes. Tweaked time to max speed on players from 0.08 to 0.07.

---
+++ Assets/Scripts/GameLogic/FinalLevelTowersScript.cs
@@ -0,0 +1,9 @@
+public static class FinalLevelTowersScript
+{
+    public static int basicTowers {get; set;} = 0;
+    public static int domTowers {get; set;} = 0;
+    public static int slowAoeTowers {get; set;} = 0;
+    public static int aoeTowers {get; set;} = 0;
+    public static int buffTowers {get; set;} = 0;
+    public static int lineAoeTowers {get; set;} = 0;
+}
